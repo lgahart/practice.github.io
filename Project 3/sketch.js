1d33f986c58d713501930c5e6cff94164515c04a
lisa <lisa.gahart@gmail.com>
2019-04-14 19:52:37 -0700

    test

---
+++ Project 3/sketch.js
@@ -0,0 +1,26 @@
+let button;
+let helloButton;
+
+let x=0;
+let exes;
+
+
+// console.log(x);
+// console.log(exes[2]);
+
+button = createButton('name/category');
+button.mousePressed(printExes);
+
+helloButton = select('#hello');
+helloButton.mousePressed(function) {
+  
+}
+
+
+
+function printExes(){
+createP(exes[2]);
+
+exes = ['bob', 'joe biden', 'henny'];
+
+}
